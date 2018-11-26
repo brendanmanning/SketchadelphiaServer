23adc7ae605a680dde33f1bb086f1ea87d5a69df
Brendan Manning <BrendanManning19380@gmail.com>
2018-11-25 22:25:34 -0500

    Updated percentile convention

--- src/main/java/com/brendanmanning/PhillyDillyDilly/Percentile.java
+++ src/main/java/com/brendanmanning/PhillyDillyDilly/Percentile.java
@@ -9,16 +9,16 @@ import com.google.gson.JsonObject;
 public class Percentile {
 
     private int percentile = -1;
-    private int lowerBound = -1;
+    private int upperBound = -1;
 
     /**
      * Creates a percentile object with a percentile and bound
      * @param percentile Ex. 10 = 10th percentile
-     * @param lowerBound All items where x >= lowerBound are in this percentile. Ex. if 70 is percentile and 50 is lowerBound, all students who scored a 50 or above are in the 70th percentile.
+     * @param upper All items where x >= lowerBound are in this percentile. Ex. if 70 is percentile and 50 is lowerBound, all students who scored a 50 or below are in the 70th percentile.
      */
-    public Percentile(int percentile, int lowerBound) {
+    public Percentile(int percentile, int upper) {
         this.percentile = percentile;
-        this.lowerBound = lowerBound;
+        this.upperBound = upper;
     }
 
     /**
@@ -30,17 +30,17 @@ public class Percentile {
     }
 
     /**
-     * Get the lower bound value
-     * @return Lower bound value
+     * Get the upper bound value
+     * @return upper bound value
      */
-    public int getLowerBound() {
-        return lowerBound;
+    public int getUpperBound() {
+        return upperBound;
     }
 
     public JsonObject toJson() {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("percentile", percentile);
-        jsonObject.addProperty("lowerBound", lowerBound);
+        jsonObject.addProperty("upperbound", upperBound);
         return jsonObject;
     }
 }
